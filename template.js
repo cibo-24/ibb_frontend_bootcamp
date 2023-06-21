e380f6a350814aa55d2ea7bca85e2225597d970b
Kenan Cibooğlu <cibo.kenan@gmail.com>
2023-06-21 19:53:22 +0300

    revize

--- template.js
+++ template.js
@@ -1,4 +1,26 @@
-let getInfo = document.querySelector('#lorem');
-getInfo.innerHTML = ('Sayfamiza Hosgeldiniz');
-console.log(getInfo.innerHTML);
+// Dark Mode
 
+let darkModu = () => {
+    alert('DARK MODU');
+    window.document.classList.toggle("dark_mode")
+}
+
+// Footer
+let newDate=()=>{
+// JS DOM
+document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+// Jquery DOM
+// const date =new Date().getFullYear();
+ // $("#date_id").html(date);
+}
+newDate();
+
+// input search
+// Jquery
+$(document).read(function(){
+    const searchApi=["Adana","Istanbul","Balikesir","Corum","Edirne","Erzincan","Tunceli"];
+    $('#tags').autocomplete({
+        source:searchApi
+    })
+}); // end
