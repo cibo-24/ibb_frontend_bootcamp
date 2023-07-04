00198dd95425a9d61e87b41dda3072c8fcefb4c7
Kenan Cibooğlu <cibo.kenan@gmail.com>
2023-07-04 19:29:24 +0300

    tutorials.js add

---
+++ js/tutorials.js
@@ -0,0 +1,19 @@
+// single Commet
+/* 
+multiple Commet
+*/
+
+console.log('loglama');
+console.warn('warn'); // dikkat işareti çıkarır
+console.error('error'); // error hatası yazdırır
+console.info('info'); // bilgi yazdırır
+
+// #####################################################
+
+alert('pop-up bilgilendirme-1'); // sayfada açılır pop-up açar
+window.alert('pop-up bilgilendirme-2'); // window nesnesi her şeyin tepesinde olan bir nesnedir.
+
+// #####################################################
+
+document.writeln('Sayın Kullanıcı:'); // document kök öğesine ulaşıp .writeln propertysi ile ekrana yazı yazdırdık.
+window.document.writeln("Eğitimimize Hoşgeldiniz!")
