f5489ffadbdd7a831d08b00b73751ed7c5629e39
Kenan Cibooğlu <cibo.kenan@gmail.com>
2023-07-04 19:41:19 +0300

    update

--- js/tutorials.js
+++ js/tutorials.js
@@ -25,4 +25,17 @@ window.document.writeln("Eğitimimize Hoşgeldiniz!")
 
 var result = 14.53;
 var result2 = "JS Öğreniyorum";
+console.log(result2);
 
+var result3 = 4423;
+console.log(result3);
+
+var result4 = true;
+console.log(typeof result4+ "->" + result); // typeof ile değişkenin tipini arattık.
+
+// Naming Convensition
+var $deneme44 ='JS Data';
+console.log($deneme44);
+
+var denemeVerisi ='JS Data';
+console.log(denemeVerisi); // camel Case >> düzenli bir isim vermektir.
